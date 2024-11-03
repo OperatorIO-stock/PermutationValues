c47fcd7e2ed9cdae12d1122b19af1b1e1c809d2e
operator_IO <vova.chigorin@yandex.ru>
2024-11-03 15:54:53 +0300

    Добавьте файлы проекта.

---
+++ PermutationValues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cupTea = "Кофе";
+            string cupCoffee = "Чай";
+
+            Console.WriteLine($"Кружка #1: {cupTea}\nКружка #2: {cupCoffee}");
+
+            string cupFree = cupCoffee;
+            cupCoffee = cupTea;
+            cupTea = cupFree;
+
+            Console.WriteLine("");
+            Console.WriteLine($"Кружка #1: {cupTea}\nКружка #2: {cupCoffee}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PermutationValues/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PermutationValues")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PermutationValues")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ba23a33c-9950-4850-b1fb-97eb235e076b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
